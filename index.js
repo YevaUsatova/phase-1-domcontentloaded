bf8bb175d1b8f6998751b7d7af659edf65eaace7
YevaUsatova <yeva.usatova@gmail.com>
2021-06-08 00:23:12 -0400

    commit tree 272ed61b6965ceeab642775bd434441d80be7061

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+ 
+
+//if (document.readyState= "complete"){
+document.addEventListener("DOMContentLoaded", function (){
+    const pText = document.querySelector('p');
+    return pText.textContent = "This is really cool!";
+});
+ //};
